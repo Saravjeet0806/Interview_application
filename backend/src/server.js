a69ba464c6c6a342e8b521b48c0d2d9cd0b1954b
Sarav <Saravjeetofficial@gmail.com>
2026-04-19 14:53:54 +0530

    clerk authentication setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import path from 'path';
 import { ENV } from './lib/env.js';
+import { connectDB } from './lib/db.js';
 
 const app = express();
 
 const __dirname = path.resolve();
-console.log('Environment Variables:', ENV);
 
 app.get('/health', (req, res) => {
     res.status(200).json({ message: 'Hello World!' });
@@ -15,13 +15,23 @@ app.get('/books', (req, res) => {
     res.status(200).json({ message: 'this is the books endpoint!' });
 });
 
-if(ENV.NODE_ENV === 'production') {
+if (ENV.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
-    app.get("/{*any}", (req, res)=>{
+    app.get("/{*any}", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     })
 }
 
-app.listen(ENV.PORT, () => {
-    console.log(`Server is running on port ${ENV.PORT}`);
-});
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () => {
+            console.log(`Server is running on port ${ENV.PORT}`);
+        });
+
+    } catch (error) {
+            console.log('Error connecting to DB', error)
+    }
+}
+
+startServer();
