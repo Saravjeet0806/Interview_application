ef5f1c2fc59d2295cf7d77a6f017e82a55fb2059
Sarav <Saravjeetofficial@gmail.com>
2026-04-25 11:24:25 +0530

    fixed bugs

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import {serve} from 'inngest/express';
 
 import { ENV } from './lib/env.js';
 import { connectDB } from './lib/db.js';
+import { inngest, functions } from "./lib/inngest.js";
 
 const app = express();
 
@@ -13,7 +14,7 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use(cors({origin:ENV.CLIENT_URL, credentials: true}));  //CROSS ORIGIN RESOURCE SHARING - to allow frontend to access backend resources , // CREDENTIALS TRUE - to allow cookies to be sent in cross origin requests
 
-app.use("/api/inngest", serve({client:inngest, functions}));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get('/health', (req, res) => {
     res.status(200).json({ message: 'Hello World!' });
