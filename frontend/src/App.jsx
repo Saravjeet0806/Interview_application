dcee9695390c56c58437cab1e03ebf37ba51c778
Saravjeet <saravjeetofficial@gmail.com>
2026-06-28 10:57:14 +0530

    fixed naming

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useUser } from "@clerk/clerk-react";
 import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
-
 import { Toaster } from "react-hot-toast";
 import DashboardPage from "./pages/DashboardPage";
 import ProblemPage from "./pages/ProblemPage";
